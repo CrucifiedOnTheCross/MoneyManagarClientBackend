ed9855ad1336b240f1c5e50b03b7b0d0ec21bd87
Silver <toooriiiiuuuuum@gmail.com>
2025-01-06 02:28:41 +0400

    Fix mapper

--- src/main/java/ru/miov/moneymanagarclientbackend/api/mapper/AccountMapper.java
+++ src/main/java/ru/miov/moneymanagarclientbackend/api/mapper/AccountMapper.java
@@ -1,15 +1,12 @@
 package ru.miov.moneymanagarclientbackend.api.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import ru.miov.moneymanagarclientbackend.api.dto.AccountDto;
 import ru.miov.moneymanagarclientbackend.store.entity.Account;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AccountMapper {
-
-    AccountMapper INSTANCE = Mappers.getMapper(AccountMapper.class);
 
     AccountDto toDto(Account account);
 
--- src/main/java/ru/miov/moneymanagarclientbackend/api/mapper/TransactionMapper.java
+++ src/main/java/ru/miov/moneymanagarclientbackend/api/mapper/TransactionMapper.java
@@ -1,15 +1,12 @@
 package ru.miov.moneymanagarclientbackend.api.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import ru.miov.moneymanagarclientbackend.api.dto.TransactionDto;
 import ru.miov.moneymanagarclientbackend.store.entity.Transaction;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface TransactionMapper {
-
-    TransactionMapper INSTANCE = Mappers.getMapper(TransactionMapper.class);
 
     TransactionDto toTransactionDto(Transaction transaction);
 
